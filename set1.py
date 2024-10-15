4bb1d48a7025f0732b04760560c6ae6b82d1e449
CHARISHMAA070217 <charishma7118@gmail.com>
2024-10-15 10:36:34 +0530

    Create set1.py

---
+++ set1.py
@@ -0,0 +1,16 @@
+'''
+set={5,3,1,4,5,2}
+a=int(input("enter number of values:"))
+for i in range(a):
+    n=int(input())
+print(*set)
+output:
+enter number of values:6
+5
+3
+1
+4
+5
+2
+1 2 3 4 5
+'''
